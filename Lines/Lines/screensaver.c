e57d069d543cdb079bafefceef49f2d84fdd3ba1
FatherFoxxy <chris.altig@gmail.com>
2016-02-28 13:37:36 -0800

    Organization/Speed improvements/added basic configuration

--- Lines/Lines/screensaver.c
+++ Lines/Lines/screensaver.c
@@ -3,26 +3,31 @@
 #include <ShlObj.h>
 #include "math_custom.h"
 #include "drawing.h"
-
-//completely arbitrary, but I don't wanna deal with lists in C
-#define MAX_DISPLAYS_SUPPORTED 5
-
-#define TARGET_TIME 0.25f
+#include "configuration.h"
 
 BOOL Running = TRUE;
 
 int monCount = 0;
+
+int ExpireCount = 0;
+
+Options options = { 0 };
 
 typedef struct tagEnumeratedDisplay
 {
 	HDC DrawingContext;
 	int ScreenWidth;
 	int ScreenHeight;
+	POINT Previous;
 	void* BackBuffer;
 	BITMAPINFO BitMapInfo;
 }EnumeratedDisplay;
 
-//Either I'm an idiot or Microsoft is an idiot, one of the two
+/* 
+   Either I'm an idiot or Microsoft is an idiot, one of the two.
+   I had to define this struct myself because MONITORINFOEX only exposed
+   the szDevice field for some reason 
+*/
 typedef struct tagMONITORINFOEX {
 	DWORD cbSize;
 	RECT  rcMonitor;
@@ -31,7 +36,7 @@ typedef struct tagMONITORINFOEX {
 	TCHAR szDevice[CCHDEVICENAME];
 } MONITORINFOEX2, *LPMONITORINFOEX2;
 
-EnumeratedDisplay Displays[MAX_DISPLAYS_SUPPORTED];
+EnumeratedDisplay* Displays;
 
 static double GTimePassed = 0;
 static float SecondsPerTick = 0;
@@ -65,7 +70,7 @@ float Sys_FloatTime()
 
 BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData)
 {
-	if (monCount > MAX_DISPLAYS_SUPPORTED) return TRUE;
+	if (monCount > options.MaxDisplaysSupported) return TRUE;
 
 	MONITORINFOEX2 info = { 0 };
 	info.cbSize = sizeof(MONITORINFOEX2);
@@ -84,6 +89,9 @@ BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMoni
 	display.ScreenHeight = screenHeight;
 	display.ScreenWidth = screenWidth;
 
+	display.Previous.x = -1;
+	display.Previous.x = -1;
+
 	int bufferSize = screenHeight*screenWidth * 4;
 	display.BackBuffer = malloc(bufferSize); //4 = bytes to display RGB
 	memset(display.BackBuffer, 0, bufferSize);
@@ -100,8 +108,67 @@ BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMoni
 	return TRUE;
 }
 
+int CalculateLines()
+{
+	if (options.ExpireDraw)
+	{
+		if (ExpireCount >= options.ExpireDrawAfter) {
+			for (int i = 0; i < monCount; i++)
+				ZeroMemory(Displays[i].BackBuffer, Displays[i].ScreenHeight*Displays[i].ScreenWidth * 4);
+			ExpireCount = 0;
+			return 0;
+		}
+		++ExpireCount;
+	}
+
+	for (int i = 0; i < monCount; i++)
+	{
+		EnumeratedDisplay display = Displays[i];
+
+		unsigned char red = rand() % 256;
+		unsigned char green = rand() % 256;
+		unsigned char blue = rand() % 256;
+		unsigned int color = (red << 16) | (green << 8) | blue;
+
+		POINT StartingPoint = { 0 };
+
+		if (options.ContinuousLines)
+		{
+			StartingPoint = display.Previous;
+		}
+
+		if (display.Previous.x <= -1 || display.Previous.y <= -1)
+		{
+			StartingPoint.x = rand() % display.ScreenWidth;
+			StartingPoint.y = rand() % display.ScreenHeight;
+		}
+
+		POINT EndingPoint = { 0 };
+		EndingPoint.x = rand() % display.ScreenWidth;
+		EndingPoint.y = rand() % display.ScreenHeight;
+
+		DrawLine(StartingPoint.x,
+				 StartingPoint.y,
+				 EndingPoint.x,
+				 EndingPoint.y,
+				 color, (int*)display.BackBuffer,
+				 display.ScreenWidth);
+
+		if (options.ContinuousLines)
+		{
+			Displays[i].Previous = EndingPoint;
+		}
+	}
+
+	return 0;
+}
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+	LoadConfig(&options);
+
+	Displays = (EnumeratedDisplay*)malloc(sizeof(EnumeratedDisplay)*options.MaxDisplaysSupported);
+	ZeroMemory(Displays, sizeof(EnumeratedDisplay)*options.MaxDisplaysSupported);
 	EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, 0);
 
 	Sys_InitFloatTime();
@@ -118,32 +185,10 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 		TimeAccumulated += NewTime - PrevTime;
 		PrevTime = NewTime;
 
-		if (TimeAccumulated > TARGET_TIME)
+		if (TimeAccumulated > options.TargetTime)
 		{
 			TimeAccumulated = 0.0f;
-
-			for (int i = 0; i < monCount; i++)
-			{
-				EnumeratedDisplay display = Displays[i];
-
-				unsigned char red = rand() % 256;
-				unsigned char green = rand() % 256;
-				unsigned char blue = rand() % 256;
-				unsigned int color = (red << 16) | (green << 8) | blue;
-
-				DrawLine(rand() % display.ScreenWidth, 
-						 rand() % display.ScreenHeight, 
-						 rand() % display.ScreenWidth,
-						 rand() % display.ScreenHeight,
-						 color, (int*)display.BackBuffer,
-						 display.ScreenWidth);
-
-				StretchDIBits(display.DrawingContext,
-					0, 0, display.ScreenWidth, display.ScreenHeight,
-					0, 0, display.ScreenWidth, display.ScreenHeight,
-					display.BackBuffer, &display.BitMapInfo,
-					DIB_RGB_COLORS, SRCCOPY);
-			}
+			CalculateLines();
 		}
 
 		for (int i = 0; i < monCount; i++)
@@ -166,6 +211,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	for (int i = 0; i < monCount; i++)
 		ReleaseDC(NULL, Displays[i].DrawingContext);
 
+	//refresh explorer to remove remanents
 	SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, 0, 0);
 
 	return EXIT_SUCCESS;
