8982263e7b707e01a667eecdecf3a6b958688728
FatherFoxxy <chris.altig@gmail.com>
2016-02-29 15:05:46 -0800

    Syntax/Beauty

--- Lines/Lines/drawing.c
+++ Lines/Lines/drawing.c
@@ -1,12 +1,12 @@
 #include "math_custom.h"
 
-int Plot(int x, int y, int color, int* buffer, int bufferWidth)
+void Plot(int x, int y, int color, int* buffer, int bufferWidth)
 {
 	buffer[(y*bufferWidth) + x] = color;
-	return 0;
 }
 
-int DrawLine(int x, int y, int x2, int y2, int color, int* buffer, int bufferWidth)
+//code credit of http://tech-algorithm.com/articles/drawing-line-using-bresenham-algorithm/
+void DrawLine(int x, int y, int x2, int y2, int color, int* buffer, int bufferWidth)
 {
 	int w = x2 - x;
 	int h = y2 - y;
@@ -16,34 +16,36 @@ int DrawLine(int x, int y, int x2, int y2, int color, int* buffer, int bufferWid
 	if (w < 0) dx2 = -1; else if (w>0) dx2 = 1;
 	int longest = Abs(w);
 	int shortest = Abs(h);
-	if (!(longest > shortest)) {
+	if (!(longest > shortest)) 
+	{
 		longest = Abs(h);
 		shortest = Abs(w);
 		if (h < 0) dy2 = -1; else if (h>0) dy2 = 1;
 		dx2 = 0;
 	}
 	int numerator = longest >> 1;
-	for (int i = 0; i <= longest; i++) {
+	for (int i = 0; i <= longest; i++) 
+	{
 		Plot(x, y, color, buffer, bufferWidth);
 		numerator += shortest;
-		if (!(numerator < longest)) {
+		if (!(numerator < longest)) 
+		{
 			numerator -= longest;
 			x += dx1;
 			y += dy1;
 		}
-		else {
+		else 
+		{
 			x += dx2;
 			y += dy2;
 		}
 	}
-	return 0;
 }
 
-int DrawRect(int x, int y, int width, int height, int color, int* buffer, int bufferWidth, int bufferHeight)
+void DrawRect(int x, int y, int width, int height, int color, int* buffer, int bufferWidth, int bufferHeight)
 {
 	for (int i = 0; i < height && x <= bufferWidth && (y+i) < bufferHeight; i++)
 	{
 		DrawLine(x, (y+i), width, (y+i), color, buffer, bufferWidth);
 	}
-	return 0;
 }
--- Lines/Lines/drawing.h
+++ Lines/Lines/drawing.h
@@ -1,5 +1,5 @@
-int Plot(int x, int y, int color, int* buffer, int bufferWidth);
+void Plot(int x, int y, int color, int* buffer, int bufferWidth);
 
-int DrawLine(int x, int y, int x2, int y2, int color, int* buffer, int bufferWidth);
+void DrawLine(int x, int y, int x2, int y2, int color, int* buffer, int bufferWidth);
 
-int DrawRect(int x, int y, int width, int height, int color, int* buffer, int bufferWidth, int bufferHeight);
+void DrawRect(int x, int y, int width, int height, int color, int* buffer, int bufferWidth, int bufferHeight);
