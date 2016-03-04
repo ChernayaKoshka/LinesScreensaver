b9322736158c74e2022a6b04481870b70de1cdf0
FatherFoxxy <chris.altig@gmail.com>
2016-03-03 18:30:16 -0800

    Added DrawTriangle/Circle/Chevron and a TRIANGLE struct

--- Lines/Lines/drawing.h
+++ Lines/Lines/drawing.h
@@ -1,5 +1,20 @@
-void Plot(int x, int y, int color, int* buffer, int bufferWidth);
+#include <Windows.h>
 
-void DrawLine(int x, int y, int x2, int y2, int color, int* buffer, int bufferWidth);
+typedef struct tagTriangle
+{
+	POINT left;
+	POINT top;
+	POINT right;
+}TRIANGLE;
+
+void Plot(int x, int y, int color, int* buffer, int bufferWidth, int bufferHeight);
+
+void DrawLine(int x, int y, int x2, int y2, int color, int* buffer, int bufferWidth, int bufferHeight);
 
 void DrawRect(int x, int y, int width, int height, int color, int* buffer, int bufferWidth, int bufferHeight);
+
+void DrawTriangle(TRIANGLE tri, int color, int* buffer, int bufferWidth, int bufferHeight);
+
+void DrawCircle(int x, int y, int radius, int color, int* buffer, int bufferWidth, int bufferHeight);
+
+void DrawChevron(int x, int y, int radius, int color, int* buffer, int bufferWidth, int bufferHeight);
